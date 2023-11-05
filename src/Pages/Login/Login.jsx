872f415e4aa61b6cdb3793311a83edbe8815a2a9
sheikhtazuddin <tazahmedsoft@gmail.com>
2023-11-05 01:47:28 -0700

    created sign in and sign up form

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
-
+import { Link } from 'react-router-dom';
+import loginbg from '../../assets/img/loginbg/loginbg.jpg'
+import { FcGoogle } from 'react-icons/fc';
 const Login = () => {
     return (
         <div>
-            <h1>login</h1>
+            <div className=" min-h-[100vh] bg-cover" style={{ backgroundImage: `url(${loginbg})` }}>
+                <div className="flex justify-center items-center min-h-[100vh] bg-[#00000062]">
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-[#ffffff71] my-10">
+                        <h1 className='flex justify-center text-4xl font-bold py-5 bg-[var(--bg-primary)] rounded-t-2xl text-white'>Sign In</h1>
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-semibold text-lg">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-semibold text-lg">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                {/* <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label> */}
+                            </div>
+                            <div className="form-control mt-6">
+                                <input type="submit" value="Sign In" className="btn btn-primary text-[17px]"/>
+                            </div>
+                        </form>
+                        <div className='flex justify-center items-center -mt-5'>
+                            <div className='bg-[var(--bg-primary)] h-[2px] w-1/4'></div>
+                            <div className='px-2'>
+                                <h1 className='text-lg font-semibold'>OR</h1>
+                            </div>
+                            <div className='bg-[var(--bg-primary)] h-[2px] w-1/4'></div>
+                        </div>
+                        <div>
+                            <div className='flex justify-center items-center gap-2 mx-8 rounded-lg border py-2 mb-3 mt-2 '>
+                            <FcGoogle className='text-2xl'></FcGoogle><span className='font-bold text-lg' >Sign In With Google</span>
+                            </div>
+                        </div>
+                        <div className='flex justify-center mb-5'>
+                            <p className='text-[18px] font-semibold'>Do't Have An Account <Link to={'/signup'} className='text-blue-700 font-bold'>Sign Up</Link></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Router/Route.jsx
+++ src/Router/Route.jsx
@@ -6,6 +6,7 @@ import Home from "../Pages/Home/Home";
 import CreateAssignments from "../Pages/CreateAssignments/CreateAssignments";
 import Assignments from "../Pages/Assignments/Assignments";
 import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
 
 
 const Route = createBrowserRouter([
@@ -28,6 +29,10 @@ const Route = createBrowserRouter([
             {
                 path:'login',
                 element:<Login></Login>
+            },
+            {
+                path:'signup',
+                element:<SignUp></SignUp>
             }
         ]
     }
